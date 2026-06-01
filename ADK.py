87e0a3933547932bdf655f4e452815b96ef10f81
AnningDan <2425400903@live.gctu.edu.gh>
2026-06-01 09:43:54 +0000

    Create ADK.py for printing KDA initials
    Added a Python script to print stylized initials for KDA with required formatting and author information.

--- ADK.py
+++ ADK.py
@@ -1,1 +1,33 @@
+# File: ADK.py
+# Description: Prints stylized large initials for KDA with required formatting.
+# Assignment Number: 1
+#
+# Name: KWADWO DAN ANNING
+# STUDENT ID: <2425400903>
+# Email: <2425400903@live.gctu.edu.gh>
+# Grader: <Augustus>
+#
+# On my honor, KWADWO DAN ANNING, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
+
+def main():
+ 
+    print()
+    print("...KDA")
+    print()
+    print("...KK..........KK......DDDDDDDD........AAAAAAA......")
+    print("...KK........KK........DDDDDDDDDD....AAAA..AAAA.....")
+    print("...KK......KK..........DD......DDD..AA........AA....")
+    print("...KK....KK............DD........DD.AA........AA....")
+    print("...KKKKKK..............DD........DD.AAAAAAAAAAAA....")
+    print("...KKKKKK..............DD........DD.AAAAAAAAAAAA....")
+    print("...KK....KK............DD........DD.AA........AA....")
+    print("...KK......KK..........DD......DDD..AA........AA..**")
+    print("...KK........KK........DDDDDDDDDD...AA........AA..**")
+    print("...KK..........KK**....DDDDDDDD....*AA........AA..**")
+    print()
+
+
+if __name__ == "__main__":
+    main()
